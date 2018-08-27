a4b63d8df90841378b69ce341dd46684f41fa2b8
Axel Kanakan <okulby@yahoo.com>
2018-08-27 11:31:47 -0700

    Trying to run eject

--- src/App.js
+++ src/App.js
@@ -78,17 +78,30 @@ class App extends Component {
         })}
         </div>
       );
+
+      style.backgroundColor = '#dd0a35';
+    }
+
+    const classes = [];
+
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
 
 
+
     return (
-      <div className="App">
-        <h1>Hello Axel</h1>
-        <p>This is my second React project</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+        <div className="App">
+          <h1>Hello Axel</h1>
+          <p className={classes.join(' ')}>This is my second React project</p>
+          <button style={style} 
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
     );
 
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,12 +3,14 @@ import './Person.css';
 
 
 
+
 const person = (props) => {
+
     return (
     <div className="Person">
         <p onClick={props.click}>Hi my name is {props.name} and I am {props.age} years old</p>
         <p>{props.children}</p>
-        <input type="text" onChange={props.changed}/>
+        <input type="text" onChange={props.changed} value={props.name}/>
     </div>);
 };
 
